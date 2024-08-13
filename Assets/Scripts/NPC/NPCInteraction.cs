42b2ca7ba7b94b4e9f00a09035efaad53f24e358
백지윤 <wendy6992@naver.com>
2024-08-13 15:54:48 +0900

    Fix: triggerarea 수정

--- Assets/Scripts/NPC/NPCInteraction.cs
+++ Assets/Scripts/NPC/NPCInteraction.cs
@@ -9,82 +9,30 @@ public class NPCInteraction : MonoBehaviour
     public GameObject dialoguePanel; // Reference to the dialogue UI panel
     public TMP_Text dialogueText; // Reference to the Text UI element
     public GameObject wheelchair; // Reference to the wheelchair
-    public GameObject Inventory; // 인벤토리 숨기기
     public Transform sitArea; // Reference to the sit area on the wheelchair
-    public Button sitWheelChairBtn; // 휠체어 태우기 버튼
-    public Button continueDialogueBtn; // 대화 계속하기 버튼
-    public Button giveItemButton; // 아이템 주기 버튼
     public float interactionDistance; // Distance to check if the wheelchair is nearby
     public bool isSittingInWheelchair = false;
     private bool playerNearby = false;
     private bool isInteracting = false;
     private int dialogueStep = 0;
-    public Animator npcAnimator;
-    public OVRInput.Button interactionButton = OVRInput.Button.Two;
-    public GameObject pickedItemPosition; // Picked item position reference
-    private InventoryManager inventoryManager; // 인벤토리 매니저 참조
 
     private string[] dialogueLines = new string[]
     {
-        "도와주세요!",
-        "폭발물에 맞아서 움직일 수가 없어요..혹시 탈 것좀 가져다 주실수 있을까요..?"
+        "Help!",
+        "I don't think I can move. Please, find something to carry me."
     };
 
     void Start()
     {
-        sitWheelChairBtn.onClick.AddListener(sitWheelChairBtnClick);
-        continueDialogueBtn.onClick.AddListener(AdvanceDialogueOrEnd); // 대화 계속하기 버튼 클릭 리스너 추가
-        continueDialogueBtn.gameObject.SetActive(false); // 시작 시 버튼 비활성화
         dialoguePanel.SetActive(false); // Initially hide the dialogue panel
-        giveItemButton.onClick.AddListener(OnGiveItemButtonClicked); // 아이템 주기 버튼 클릭 리스너 추가
-        giveItemButton.gameObject.SetActive(false); // 시작 시 버튼 비활성화
-        inventoryManager = FindObjectOfType<InventoryManager>(); // InventoryManager 찾기
+        AddEventTriggerListener(dialoguePanel, EventTriggerType.PointerClick, OnDialoguePanelClick);
     }
 
     void Update()
     {
-        if (playerNearby && OVRInput.GetDown(interactionButton))
+        if (playerNearby && Input.GetKeyDown(KeyCode.E) && !isInteracting)
         {
-            if (!isInteracting)
-            {
-                StartDialogue();
-            }
-            else
-            {
-                AdvanceDialogueOrEnd();
-            }
-        }
-
-        if (IsWheelchairNearby() && playerNearby && !isSittingInWheelchair)
-        {
-            sitWheelChairBtn.gameObject.SetActive(true);
-        }
-        else
-        {
-            sitWheelChairBtn.gameObject.SetActive(false);
-        }
-
-        // NPC가 근처에 있고, 플레이어가 아이템을 들고 있을 때 아이템 주기 버튼 활성화
-        if (playerNearby && pickedItemPosition.transform.childCount > 0)
-        {
-            GameObject pickedItem = pickedItemPosition.transform.GetChild(0).gameObject;
-            string pickedItemName = pickedItem.name;
-            Debug.Log($"{pickedItemName}");
-
-            if (pickedItemName == "NSAIDs(Clone)" || pickedItemName == "연골보호제(Clone)")
-            {
-                string displayName = pickedItemName.Replace("(Clone)", "").Trim();
-                giveItemButton.gameObject.SetActive(true);
-                giveItemButton.GetComponentInChildren<TMP_Text>().text = $"{displayName} 투여하기";
-            }
-            else
-            {
-                giveItemButton.gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            giveItemButton.gameObject.SetActive(false);
+            StartDialogue();
         }
     }
 
@@ -111,8 +59,6 @@ public class NPCInteraction : MonoBehaviour
         isInteracting = true;
         dialogueStep = 0;
         dialoguePanel.SetActive(true);
-        continueDialogueBtn.gameObject.SetActive(true); // 대화 계속하기 버튼 활성화
-        Inventory.SetActive(false); // Hide the inventory during the dialogue
         dialogueText.text = dialogueLines[dialogueStep];
     }
 
@@ -133,25 +79,7 @@ public class NPCInteraction : MonoBehaviour
     {
         isInteracting = false;
         dialoguePanel.SetActive(false);
-        continueDialogueBtn.gameObject.SetActive(false); // 대화 계속하기 버튼 비활성화
-        Inventory.SetActive(true); // Show the inventory after the dialogue ends
-        Debug.Log("Find something to carry the NPC");
-    }
-
-    private void AdvanceDialogueOrEnd()
-    {
-        if (dialogueStep < dialogueLines.Length - 1)
-        {
-            AdvanceDialogue();
-        }
-        else
-        {
-            EndDialogue();
-        }
-    }
-
-    private void sitWheelChairBtnClick()
-    {
+        Debug.Log("Find something to carry the NPC.");
         if (wheelchair != null && IsWheelchairNearby())
         {
             SitInWheelchair();
@@ -170,8 +98,6 @@ public class NPCInteraction : MonoBehaviour
         transform.rotation = sitArea.rotation;
         transform.SetParent(wheelchair.transform);
         isSittingInWheelchair = true;
-        npcAnimator.SetBool("Laying", false);
-        sitWheelChairBtn.gameObject.SetActive(false);
         Debug.Log("NPC is now in the wheelchair.");
     }
 
@@ -182,7 +108,6 @@ public class NPCInteraction : MonoBehaviour
         transform.position = bedTransform.position;
         transform.rotation = bedTransform.rotation;
         isSittingInWheelchair = false;
-        npcAnimator.SetBool("Laying", true);
         Debug.Log("NPC is now on the bed.");
     }
 
@@ -190,7 +115,7 @@ public class NPCInteraction : MonoBehaviour
     {
         if (isInteracting)
         {
-            AdvanceDialogueOrEnd();
+            AdvanceDialogue();
         }
     }
 
@@ -204,32 +129,5 @@ public class NPCInteraction : MonoBehaviour
         EventTrigger.Entry entry = new EventTrigger.Entry { eventID = type };
         entry.callback.AddListener(action);
         trigger.triggers.Add(entry);
-    }
-
-    private void OnGiveItemButtonClicked()
-    {
-        if (pickedItemPosition.transform.childCount > 0)
-        {
-            GameObject pickedItem = pickedItemPosition.transform.GetChild(0).gameObject;
-            string pickedItemName = pickedItem.name;
-
-            if (pickedItemName == "NSAIDs(Clone)" || pickedItemName == "연골보호제(Clone)")
-            {
-                Debug.Log($"{pickedItemName.Replace("(Clone)", "").Trim()} 투여 완료");
-                string itemName = pickedItemName.Replace("(Clone)", "").Trim();
-
-                // 인벤토리 매니저에서 해당 아이템 제거
-                if (itemName == "NSAIDs")
-                {
-                    inventoryManager.RemoveItemByName("NSAIDs");
-                }
-                else if (itemName == "연골보호제")
-                {
-                    inventoryManager.RemoveItemByName("Chondroprotective agents");
-                }
-
-                Destroy(pickedItem); // 아이템 오브젝트 삭제
-            }
-        }
     }
 }
--- Assets/Scripts/Object/TriggerArea.cs
+++ Assets/Scripts/Object/TriggerArea.cs
@@ -6,38 +6,35 @@ public class TriggerArea : MonoBehaviour
     public Camera mainCamera;
     public Transform screenViewTransform;
     public Transform originalViewTransform; // 카메라의 원래 위치와 회전
-
+    public MonoBehaviour cameraControlScript; // 카메라 제어 스크립트 참조
+    public PlayerController playerController; // PlayerController 참조
     public ScreenUIManager screenUIManager; // ScreenUIManager 참조
     public GameObject interactionPrompt; // Interaction prompt 참조
-    public GameObject otherCanvas; // 다른 캔버스 참조 (숨길/보일 캔버스)
-
     private bool isPlayerInRange = false;
     private bool isInteracting = false;
-    public OVRInput.Button interactionButton = OVRInput.Button.One;
 
     void Start()
     {
         if (mainCamera == null) Debug.LogError("mainCamera is not assigned.");
         if (screenViewTransform == null) Debug.LogError("screenViewTransform is not assigned.");
         if (originalViewTransform == null) Debug.LogError("originalViewTransform is not assigned.");
+        if (cameraControlScript == null) Debug.LogError("cameraControlScript is not assigned.");
+        if (playerController == null) Debug.LogError("playerController is not assigned.");
         if (screenUIManager == null) Debug.LogError("screenUIManager is not assigned.");
         if (interactionPrompt == null) Debug.LogError("interactionPrompt is not assigned.");
-        if (otherCanvas == null) Debug.LogError("otherCanvas is not assigned.");
 
         interactionPrompt.SetActive(false); // 시작 시 텍스트 비활성화
-        otherCanvas.SetActive(true); // 시작 시 다른 캔버스 활성화 (필요에 따라 조정)
     }
 
     void Update()
     {
-        if (mainCamera == null || screenViewTransform == null || originalViewTransform == null || screenUIManager == null || interactionPrompt == null || otherCanvas == null)
+        if (mainCamera == null || screenViewTransform == null || originalViewTransform == null || playerController == null || screenUIManager == null || interactionPrompt == null)
         {
             return;
         }
 
-        if (isPlayerInRange && OVRInput.GetDown(interactionButton))
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("상호작용버튼 클릭!!!");
             if (isInteracting)
             {
                 ExitInteraction();
@@ -53,9 +50,9 @@ public class TriggerArea : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("플레이어 발견!!!");
             isPlayerInRange = true;
             interactionPrompt.SetActive(true); // 텍스트 활성화
+            Debug.Log("텍스트 활성화");
         }
     }
 
@@ -63,9 +60,10 @@ public class TriggerArea : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("플레이어 나감!!!");
             isPlayerInRange = false;
             interactionPrompt.SetActive(false); // 텍스트 비활성화
+            Debug.Log("텍스트 비활성화");
+
         }
     }
 
@@ -79,8 +77,8 @@ public class TriggerArea : MonoBehaviour
         mainCamera.transform.LookAt(screenViewTransform);
 
         LockCameraControl();
+        playerController.EnterInteractionMode();
         screenUIManager.ShowScreenUI(); // UI 활성화
-        otherCanvas.SetActive(false); // 다른 캔버스 숨기기
         interactionPrompt.SetActive(false); // 텍스트 비활성화
         isInteracting = true;
         Debug.Log("상호작용 시작");
@@ -92,8 +90,8 @@ public class TriggerArea : MonoBehaviour
         mainCamera.transform.position = originalViewTransform.position;
         mainCamera.transform.rotation = originalViewTransform.rotation;
         UnlockCameraControl();
+        playerController.ExitInteractionMode();
         screenUIManager.HideScreenUI(); // UI 비활성화
-        otherCanvas.SetActive(true); // 다른 캔버스 다시 활성화
         interactionPrompt.SetActive(false); // 텍스트 비활성화
         isInteracting = false;
         Debug.Log("상호작용 종료");
@@ -102,12 +100,21 @@ public class TriggerArea : MonoBehaviour
     void LockCameraControl()
     {
         // 카메라 제어 비활성화
+        if (cameraControlScript != null)
+        {
+            cameraControlScript.enabled = false;
+        }
         Cursor.lockState = CursorLockMode.None;
     }
 
     void UnlockCameraControl()
     {
         // 카메라 제어 활성화
+        if (cameraControlScript != null)
+        {
+            cameraControlScript.enabled = true;
+        }
         // 커서 잠금 상태 설정 부분을 제거합니다.
+        // Cursor.lockState = CursorLockMode.Locked;
     }
 }
