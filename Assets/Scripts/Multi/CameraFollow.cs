9a451be59200cc06940e0fa9d16edf7400c8a072
백지윤 <wendy6992@naver.com>
2024-08-17 13:46:24 +0900

    Fix: 카메라 문제 수정

--- Assets/Scripts/Multi/CameraFollow.cs
+++ Assets/Scripts/Multi/CameraFollow.cs
@@ -2,41 +2,29 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public static CameraFollow Singleton
-    {
-        get => _singleton;
-        set
-        {
-            if (value == null)
-                _singleton = null;
-            else if (_singleton == null)
-                _singleton = value;
-            else if (_singleton != value)
-            {
-                Destroy(value);
-                Debug.LogError($"There should only ever be one instance of {nameof(CameraFollow)}!");
-            }
-        }
-    }
-    private static CameraFollow _singleton;
+    public static CameraFollow Singleton { get; private set; }
 
     private Transform target;
 
     private void Awake()
     {
-        Singleton = this;
-    }
-
-    private void OnDestroy()
-    {
-        if (Singleton == this)
-            Singleton = null;
+        if (Singleton == null)
+        {
+            Singleton = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("There should only ever be one instance of CameraFollow!");
+        }
     }
 
     private void LateUpdate()
     {
         if (target != null)
+        {
             transform.SetPositionAndRotation(target.position, target.rotation);
+        }
     }
 
     public void SetTarget(Transform newTarget)
