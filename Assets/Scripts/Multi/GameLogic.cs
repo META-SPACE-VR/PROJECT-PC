ab3d7904fb525a439b8cc3849367f596f82cb659
이민지 <50236187+namoo1818@users.noreply.github.com>
2024-08-17 14:28:34 +0900

    feat: Game씬에 우주선 복사 및 스폰 지점 변경

--- Assets/Scripts/Multi/GameLogic.cs
+++ Assets/Scripts/Multi/GameLogic.cs
@@ -6,13 +6,15 @@ public class GameLogic : NetworkBehaviour, IPlayerJoined, IPlayerLeft
 {
     [SerializeField] private NetworkPrefabRef playerPrefab;
     [Networked, Capacity(4)] private NetworkDictionary<PlayerRef, Player> Players => default;
-    
+    public Transform spawnPosition;  // 스폰 위치를 지정할 Transform
 
     public void PlayerJoined(PlayerRef player)
     {
         if (HasStateAuthority)
         {
-            NetworkObject playerObject = Runner.Spawn(playerPrefab, Vector3.up, Quaternion.identity, player);
+            // Transform의 위치 값을 Vector3로 변환하여 전달
+            Vector3 spawnPos = spawnPosition.position;
+            NetworkObject playerObject = Runner.Spawn(playerPrefab, spawnPos, Quaternion.identity, player);
             Players.Add(player, playerObject.GetComponent<Player>());
         }
     }
