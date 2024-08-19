0a5c40a7261560222bbb409a1d783877faacab43
aycho00 <aycho3030@gmail.com>
2024-08-19 15:27:07 +0900

    feat: pregame(lobby) scene add

--- Assets/Scripts/Multi/GameStartManager.cs
+++ Assets/Scripts/Multi/GameStartManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Fusion;
 using FusionExamples.Utility;
-using TMPro.TextMeshProUGUI;
 
 namespace Managers
 {
@@ -13,21 +13,17 @@ namespace Managers
         public const int LAUNCH_SCENE = 0;
 		public const int LOBBY_SCENE = 1;
         public const int GAME_SCENE = 2; 
-        public TMP_Button StartBtn; 
+        public GameObject StartBtnCanvas;
+        public Button StartBtn; 
         
         public static GameStartManager Instance => Singleton<GameStartManager>.Instance;
         
-        void Awake()
-        {
-            // 현재 씬의 인덱스를 얻기
-            Scene currentScene = SceneManager.GetActiveScene();
-            int sceneIndex = currentScene.buildIndex;
-            // if (sceneIndexIndex==1) StartBtn.SetActive(true); 
-        }
-
         public void onClickStartBtn()
         {
+            if (Instance.Runner == null) return;
+
             Instance.Runner.LoadScene(SceneRef.FromIndex(GAME_SCENE));
+            StartBtnCanvas.SetActive(false);
         }
     }
 }
